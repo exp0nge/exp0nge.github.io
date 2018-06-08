2b468c8b5f1ddeea8660300160aa6f188b5c9894
exp0nge <mdislamwork@gmail.com>
2018-06-08 00:05:49 -0400

    Update exp

--- src/data/experience.tsx
+++ src/data/experience.tsx
@@ -4,7 +4,12 @@ export const EXPERIENCE = [{
     'location': 'Yorktown Heights, NY',
     'startDate': 'September 2018',
     'endDate': 'Present',
-    'description': ['Ongoing']
+    'description': [
+        'Developing automated machine learning system to win data science competitions',
+        'Maintaining and supporting booth display frontend for major conferences remotely',
+        'Developed and deployed the backend for a web app for NIPS 2017 to show bias in AI and ' +
+        'also collect data from interactions by users and which is available to the public today'
+    ]
 },
 {
     'company': 'Appfigures',
