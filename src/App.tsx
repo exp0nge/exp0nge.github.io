559d33d35394ea9127f6a54ab56cf997675a8f52
exp0nge <mdislamwork@gmail.com>
2018-06-08 01:31:59 -0400

    Use SideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Layout, Menu, Breadcrumb, Card, Row, Col, List, Avatar, Icon } from 'antd';
-const { Header, Content, Footer } = Layout;
+const { Sider, Content, Footer } = Layout;
 const { Meta } = Card;
 
 import './App.css';
@@ -24,15 +24,38 @@ const pagination = {
 };
 
 class App extends React.Component {
+  state = {
+    contentMarginLeft: 200
+  };
+
+  sidebarCollapsed = (collapsed: boolean) => {
+    if (collapsed) {
+      this.setState({
+        contentMarginLeft: 0
+      });
+    } else {
+      this.setState({
+        contentMarginLeft: 200
+      });
+    }
+  }
 
   render() {
+
+    const { contentMarginLeft } = this.state;
+
     return (
       <Layout>
-        <Header style={{ position: 'fixed', width: '100%', zIndex: 10000 }}>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}
+          onCollapse={this.sidebarCollapsed}
+        >
           <div className="logo" />
           <Menu
             theme="dark"
-            mode="horizontal"
+            mode="inline"
             defaultSelectedKeys={['0']}
             style={{ lineHeight: '64px' }}
           >
@@ -47,137 +70,138 @@ class App extends React.Component {
               </a>
             </Menu.Item>
           </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px', marginTop: 64 }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Portfolio</Breadcrumb.Item>
-            <Breadcrumb.Item><a id="about">About</a></Breadcrumb.Item>
-          </Breadcrumb>
-          <Row gutter={16}>
-            <Col xs={{ span: 8, offset: 8 }} lg={{ span: 4, offset: 0 }}>
-              <Card
-                hoverable={false}
-                style={{ width: '100%' }}
-                cover={<img alt="example" src={headshotPicture} />}
-              >
-                <Meta
-                  title="MD R. Islam"
-                  description="Software Engineer @ IBM"
-                />
-              </Card>
-            </Col>
-            <Col xs={{ span: 24, offset: 0 }} lg={{ span: 20, offset: 0 }}>
-              <Row gutter={16}>
-                <Col xs={{ span: 12, offset: 6 }} lg={{ span: 8, offset: 0 }}>
-                  <Card title="Education" bordered={true} style={{ width: '100%' }}>
-                    <List.Item>
-                      <List.Item.Meta
-                        // avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                        title="B.S. in Computer Science, Magna Cum Laude"
-                        description="The City College of New York - CUNY"
-                      />
-                    </List.Item>
-                  </Card>
-                </Col>
-                <Col xs={{ span: 24, offset: 0 }} lg={{ span: 16, offset: 0 }}>
-                  <Card title="Technical Skills" bordered={true} style={{ width: '100%' }}>
-                    <List
-                      grid={{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 4, xxl: 4 }}
-                      itemLayout="horizontal"
-                      dataSource={SKILLS}
-                      renderItem={(item: { [index: string]: string }) => (
-                        <List.Item>
-                          <List.Item.Meta
-                            avatar={<Avatar src={item.img} />}
-                            title={item.name}
-                          />
-                        </List.Item>
-                      )}
+        </Sider>
+        <Layout style={{ marginLeft: contentMarginLeft }}>
+          <Content style={{ padding: '0 50px', marginTop: 64 }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Portfolio</Breadcrumb.Item>
+              <Breadcrumb.Item><a id="about">About</a></Breadcrumb.Item>
+            </Breadcrumb>
+            <Row gutter={16}>
+              <Col xs={{ span: 8 }} lg={{ span: 8, offset: 0 }}>
+                <Card
+                  hoverable={false}
+                  style={{ width: '100%' }}
+                  cover={<img alt="example" src={headshotPicture} />}
+                >
+                  <Meta
+                    title="MD R. Islam"
+                    description="Software Engineer @ IBM"
+                  />
+                </Card>
+              </Col>
+              <Col xs={{ span: 16 }} lg={{ span: 16, offset: 0 }}>
+                <Card title="Education" bordered={true} style={{ width: '100%' }}>
+                  <List.Item>
+                    <List.Item.Meta
+                      // avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                      title="B.S. in Computer Science, Magna Cum Laude"
+                      description={['The City College of New York - CUNY', 
+                      <br key="eduBr" />, 
+                      'May 2017']}
                     />
-                  </Card>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Content>
+                  </List.Item>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={{ span: 24, offset: 0 }} lg={{ span: 24, offset: 0 }}>
+                <Card title="Technical Skills" bordered={true} style={{ width: '100%' }}>
+                  <List
+                    grid={{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 4, xxl: 4 }}
+                    itemLayout="horizontal"
+                    dataSource={SKILLS}
+                    renderItem={(item: { [index: string]: string }) => (
+                      <List.Item>
+                        <List.Item.Meta
+                          avatar={<Avatar src={item.img} />}
+                          title={item.name}
+                        />
+                      </List.Item>
+                    )}
+                  />
+                </Card>
+              </Col>
+            </Row>
+          </Content>
 
-        <Content style={{ padding: '0 50px', marginTop: 64 }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Portfolio</Breadcrumb.Item>
-            <Breadcrumb.Item><a id="experience">Experience</a></Breadcrumb.Item>
-          </Breadcrumb>
-          {
-            EXPERIENCE.map((item, i) =>
-              <div key={i}>
-                <Row>
-                  <Col span={12} offset={6}>
-                    <Card
-                      title={item.position +
-                        ' @ ' +
-                        item.company +
-                        ' / ' +
-                        item.location}
-                      bordered={false}
-                      style={{ width: '100%' }}
-                      extra={item.startDate + ' - ' + item.endDate}
-                    >
-                      {item.description.map((itemDescription, descId) =>
-                        <p key={descId}>- {itemDescription}</p>
-                      )}
-                    </Card>
-                  </Col>
-                </Row>
-                <div style={{ marginBottom: 16 }} />
-              </div>
-            )
-          }
+          <Content style={{ padding: '0 50px', marginTop: 64 }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Portfolio</Breadcrumb.Item>
+              <Breadcrumb.Item><a id="experience">Experience</a></Breadcrumb.Item>
+            </Breadcrumb>
+            {
+              EXPERIENCE.map((item, i) =>
+                <div key={i}>
+                  <Row>
+                    <Col xs={{ span: 24, offset: 0 }} lg={{ span: 12, offset: 6 }}>
+                      <Card
+                        title={item.position +
+                          ' @ ' +
+                          item.company +
+                          ' / ' +
+                          item.location}
+                        bordered={false}
+                        style={{ width: '100%' }}
+                        extra={item.startDate + ' - ' + item.endDate}
+                      >
+                        {item.description.map((itemDescription, descId) =>
+                          <p key={descId}>- {itemDescription}</p>
+                        )}
+                      </Card>
+                    </Col>
+                  </Row>
+                  <div style={{ marginBottom: 16 }} />
+                </div>
+              )
+            }
 
-        </Content>
+          </Content>
 
-        <Content style={{ padding: '0 50px', marginTop: 64 }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Portfolio</Breadcrumb.Item>
-            <Breadcrumb.Item><a id="projects">Projects</a></Breadcrumb.Item>
-          </Breadcrumb>
+          <Content style={{ padding: '0 50px', marginTop: 64 }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Portfolio</Breadcrumb.Item>
+              <Breadcrumb.Item><a id="projects">Projects</a></Breadcrumb.Item>
+            </Breadcrumb>
 
-          <Row gutter={16}>
-            <Col xs={{ span: 24, offset: 0 }} lg={{ span: 16, offset: 4 }}>
-              <Card title="Projects" bordered={true} style={{ width: '100%' }}>
-                <List
-                  itemLayout="vertical"
-                  size="large"
-                  pagination={pagination}
-                  dataSource={PROJECTS}
-                  renderItem={(item: { [index: string]: string }) => (
-                    <List.Item
-                      key={item.title}
-                      actions={[<IconText type="arrow-right" text="Visit" key={item.title} link={item.link} />]}
-                      extra={<img
-                        width={150}
-                        height={150}
-                        alt="logo"
-                        src={item.href}
-                      />}
-                    >
-                      <List.Item.Meta
-                        avatar={<Avatar src={item.avatar} />}
-                        title={<a href={item.href}>{item.title}</a>}
-                        description={item.description}
-                      />
-                      {item.content}
-                    </List.Item>
-                  )}
-                />
-              </Card>
-            </Col>
-          </Row>
+            <Row gutter={16}>
+              <Col xs={{ span: 24, offset: 0 }} lg={{ span: 16, offset: 4 }}>
+                <Card title="Projects" bordered={true} style={{ width: '100%' }}>
+                  <List
+                    itemLayout="vertical"
+                    size="large"
+                    pagination={pagination}
+                    dataSource={PROJECTS}
+                    renderItem={(item: { [index: string]: string }) => (
+                      <List.Item
+                        key={item.title}
+                        actions={[<IconText type="arrow-right" text="Visit" key={item.title} link={item.link} />]}
+                        extra={<img
+                          width={150}
+                          height={150}
+                          alt="logo"
+                          src={item.href}
+                        />}
+                      >
+                        <List.Item.Meta
+                          avatar={<Avatar src={item.avatar} />}
+                          title={<a href={item.href}>{item.title}</a>}
+                          description={item.description}
+                        />
+                        {item.content}
+                      </List.Item>
+                    )}
+                  />
+                </Card>
+              </Col>
+            </Row>
 
-        </Content>
-
-        <Footer style={{ textAlign: 'center' }}>
-          &copy; MD R Islam 2018 | Based off of Ant Design
-    </Footer>
-      </Layout >
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            &copy; MD R Islam 2018 | Based off of Ant Design
+      </Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
