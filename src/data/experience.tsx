c459de3703e69f0f79137c9988c82d01259be578
exp0nge <mdislamwork@gmail.com>
2018-09-09 21:40:28 -0400

    unregister service workeR

--- src/data/experience.tsx
+++ src/data/experience.tsx
@@ -1,78 +1,77 @@
 export const EXPERIENCE = [
     {
-    'company': 'Gemini.com',
-    'position': 'Software Engineer',
-    'location': 'New York, NY',
-    'startDate': 'September 2019',
-    'endDate': 'Present',
-    'description': [
-        ''
-    ]
-    },   
- {
-    'company': 'IBM',
-    'position': 'Software Engineer',
-    'location': 'Yorktown Heights, NY',
-    'startDate': 'August 2018',
-    'endDate': 'August 2019',
-    'description': [
-        'Developed automated machine learning system to win data science competitions',
-        'Maintained and supporting booth display frontend for major conferences remotely',
-        'Developed and deployed the backend for a web app for NIPS 2017 to show bias in AI and ' +
-        'also collect data from interactions by users and which is available to the public today'
-    ]
-},
-{
-    'company': 'Appfigures',
-    'position': 'Backend Developer Intern',
-    'location': 'New York, NY',
-    'startDate': 'January 2017',
-    'endDate': 'May 2017',
-    'description': [
-        'Integrated various ad networks and usage networks onto the platform using C#/.NET',
-        'Created news scraper with Python to extract app mentions from different news sources',
-        'Added analytics support for ad spend and integrated a few networks for the feature'
-    ]
-},
-{
-    'company': 'Gleam',
-    'position': 'Software Engineer',
-    'location': 'New York, NY',
-    'startDate': 'December 2015',
-    'endDate': 'January 2017',
-    'description': [
-        'Developed and maintained chat, data visualization, task, journal, and note management ' +
-        'system for patient and therapist views using the Django framework which create more ' +
-        'effective and efficient mental healthcare therapy sessions',
-        'Researched, prototyped, and implemented user stories in an Agile team',
-        'Utilized dc.js with D3.js for interactive data visualizations using AlchemyAPI for ' +
-        'sentiment analysis'
-    ]
-},
-{
-    'company': 'JPMorgan Chase & Co.',
-    'position': 'Application Developer Intern',
-    'location': 'Brooklyn, NY',
-    'startDate': 'June 2016',
-    'endDate': 'August 2016',
-    'description': [
-        'Developed backend for an attestation feature for capital using the Athena framework',
-        'Utilized Python, relational and object-orientated databases, in an Agile environment ' +
-        'alongside four other developers',
-        'Replaced existing user tool to centralize and standardize the workflow for corporate line of ' +
-        'business controllers to reduce risk'
-    ]
-},
-{
-    'company': 'Los Alamos National Laboratory',
-    'position': 'Software Developer Intern',
-    'location': 'Los Alamos, NM',
-    'startDate': 'June 2015',
-    'endDate': 'August 2015',
-    'description': [
-        'Developed a cross-platform GUI to allow computational earth scientists to simulate ' +
-        'subsurface flow and transport models using the PySide (Qt wrapper) framework',
-        'Optimized the workflow for scientists by allowing them to rapidly model systems'
-    ]
-}
+        'company': 'Gemini.com',
+        'position': 'Software Engineer',
+        'location': 'New York, NY',
+        'startDate': 'September 2019',
+        'endDate': 'Present',
+        'description': [
+        ]
+    },
+    {
+        'company': 'IBM',
+        'position': 'Software Engineer',
+        'location': 'Yorktown Heights, NY',
+        'startDate': 'August 2018',
+        'endDate': 'August 2019',
+        'description': [
+            'Developed automated machine learning system to win data science competitions',
+            'Maintained and supporting booth display frontend for major conferences remotely',
+            'Developed and deployed the backend for a web app for NIPS 2017 to show bias in AI and ' +
+            'also collect data from interactions by users and which is available to the public today'
+        ]
+    },
+    {
+        'company': 'Appfigures',
+        'position': 'Backend Developer Intern',
+        'location': 'New York, NY',
+        'startDate': 'January 2017',
+        'endDate': 'May 2017',
+        'description': [
+            'Integrated various ad networks and usage networks onto the platform using C#/.NET',
+            'Created news scraper with Python to extract app mentions from different news sources',
+            'Added analytics support for ad spend and integrated a few networks for the feature'
+        ]
+    },
+    {
+        'company': 'Gleam',
+        'position': 'Software Engineer',
+        'location': 'New York, NY',
+        'startDate': 'December 2015',
+        'endDate': 'January 2017',
+        'description': [
+            'Developed and maintained chat, data visualization, task, journal, and note management ' +
+            'system for patient and therapist views using the Django framework which create more ' +
+            'effective and efficient mental healthcare therapy sessions',
+            'Researched, prototyped, and implemented user stories in an Agile team',
+            'Utilized dc.js with D3.js for interactive data visualizations using AlchemyAPI for ' +
+            'sentiment analysis'
+        ]
+    },
+    {
+        'company': 'JPMorgan Chase & Co.',
+        'position': 'Application Developer Intern',
+        'location': 'Brooklyn, NY',
+        'startDate': 'June 2016',
+        'endDate': 'August 2016',
+        'description': [
+            'Developed backend for an attestation feature for capital using the Athena framework',
+            'Utilized Python, relational and object-orientated databases, in an Agile environment ' +
+            'alongside four other developers',
+            'Replaced existing user tool to centralize and standardize the workflow for corporate line of ' +
+            'business controllers to reduce risk'
+        ]
+    },
+    {
+        'company': 'Los Alamos National Laboratory',
+        'position': 'Software Developer Intern',
+        'location': 'Los Alamos, NM',
+        'startDate': 'June 2015',
+        'endDate': 'August 2015',
+        'description': [
+            'Developed a cross-platform GUI to allow computational earth scientists to simulate ' +
+            'subsurface flow and transport models using the PySide (Qt wrapper) framework',
+            'Optimized the workflow for scientists by allowing them to rapidly model systems'
+        ]
+    }
 ];
